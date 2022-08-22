c49516221a6c6d773c3ad2c069fea8bdcde01c99
wumiao5s <15950060547@163.com>
2022-08-22 15:07:28 +0800

    添加文件表单请求HttpRequestFileForm

---
+++ src/Feign/Request/HttpRequestFileForm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Feign.Request
+{
+    /// <summary>
+    /// 文件表单请求
+    /// </summary>
+    public class HttpRequestFileForm : IHttpRequestFileForm
+    {
+        public HttpRequestFileForm() { }
+
+        public HttpRequestFileForm(IHttpRequestFile requestFile)
+        {
+            RequestFiles = new List<IHttpRequestFile>() { requestFile };
+        }
+
+        public HttpRequestFileForm(IEnumerable<IHttpRequestFile> requestFiles)
+        {
+            RequestFiles = requestFiles;
+        }
+
+        public IEnumerable<IHttpRequestFile> RequestFiles { get; set; }
+    }
+}
