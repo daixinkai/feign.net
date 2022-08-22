53e91eb119fe045c97e48c96413dc2336325bfed
wumiao5s <15950060547@163.com>
2022-08-22 15:17:56 +0800

    添加Patch请求Attribute

---
+++ src/Feign/PatchMappingAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feign
+{
+    /// <summary>
+    /// 映射一个PATCH请求
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class PatchMappingAttribute : RequestMappingBaseAttribute
+    {
+        public PatchMappingAttribute() { }
+        public PatchMappingAttribute(string value) : base(value)
+        {
+        }
+
+        public override string GetMethod()
+        {
+            return "PATCH";
+        }
+    }
+}
